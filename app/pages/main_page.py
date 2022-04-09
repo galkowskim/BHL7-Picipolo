dde4e6e88b60ab4704dc01e57027bc5beff3b28b
hbujakow <72279632+hbujakow@users.noreply.github.com>
2022-04-09 13:23:05 +0200

    Update app

--- app/pages/main_page.py
+++ app/pages/main_page.py
@@ -9,18 +9,21 @@ def app():
 
     longitude = st.text_input('Enter longitude coordinates of the property', '40.70')
     latitude = st.text_input('Enter latitude coordinates of the property', '60.30')
-    region = st.text_input('Enter area region of NYC the property is located in', 'Bronx')
+    region = st.selectbox(
+     'Select area region of NYC the property is located in',
+     ('Brooklyn', 'Bronx', 'Manhattan', 'Queens', 'Staten Island'))
     year = st.text_input('Enter year the property was built in', '1997')
-    x = pd.DataFrame(columns = ['EXT_E','EXMPTCL_X1','EXMPTCL_X5','YEAR_2018','Borough_BRONX','Borough_BROOKLYN','Borough_MANHATTAN','Borough_STATEN IS',
-'BLDGCL_B3','BLDGCL_C0','BLDGCL_Other','BLDGCL_V0','TAXCLASS_1','TAXCLASS_1A','TAXCLASS_1C','TAXCLASS_2','TAXCLASS_2A',
-'TAXCLASS_2B','TAXCLASS_2C','TAXCLASS_4','STORIES','AVTOT','EXTOT','EXCD1','Latitude','Longitude','LOTAREA','BLDAREA'])
-    x.loc[0] = [0,0,0, 1 if year == 2018 else 0,1 if region == "Bronx" else 0, 1 if region == "Brooklyn" else 0,
+    
+    loaded_model = scraper.load_model()
+    if(st.button("Predict")):
+        
+        x = pd.DataFrame(columns = ['EXT_E','EXMPTCL_X1','EXMPTCL_X5','YEAR_2018','Borough_BRONX','Borough_BROOKLYN','Borough_MANHATTAN','Borough_STATEN IS',
+    'BLDGCL_B3','BLDGCL_C0','BLDGCL_Other','BLDGCL_V0','TAXCLASS_1','TAXCLASS_1A','TAXCLASS_1C','TAXCLASS_2','TAXCLASS_2A',
+    'TAXCLASS_2B','TAXCLASS_2C','TAXCLASS_4','STORIES','AVTOT','EXTOT','EXCD1','Latitude','Longitude','LOTAREA','BLDAREA'])
+        x.loc[0] = [0,0,0, 1 if year == 2018 else 0,1 if region == "Bronx" else 0, 1 if region == "Brooklyn" else 0,
                 1 if region == "Manhattan" else 0, 1 if region == "Staten Island" else 0,0.017101,0.046281,0.353863,0.017340,
-0.499402,0.097345,0.000478,0.218608,0.019373,0.010883,0.018775,0.111337,0.119540,0.001942,0.001681,0.107027,latitude,longitude, 0.145783, 0.105869]
+            0.499402,0.097345,0.000478,0.218608,0.019373,0.010883,0.018775,0.111337,0.119540,0.001942,0.001681,0.107027,latitude,longitude, 0.145783, 0.105869]
+        result = loaded_model.predict(x)
+     # próba - potem sie usunie i zamieni wynikiem z modelu
 
-    filename = 'finalizedmodel.sav'
-    loaded_model = pickle.load(open(filename, 'rb'))
-
-    result = loaded_model.predict(x) # próba - potem sie usunie i zamieni wynikiem z modelu
-
-    price = st.text(f"Predicted price of your real estate is: {result}")
+        st.text(f"Predicted price of your real estate is: {int(result)}$")
--- app/pages/utils/scraper.py
+++ app/pages/utils/scraper.py
@@ -1,8 +1,16 @@
 from pathlib import Path
 import pandas as pd
 from typing import Optional
+import pickle
 
-
+def load_model():
+    file_path = Path(__file__).resolve()
+    data_path = file_path.parents[3].joinpath('AI','finalizedmodel.sav')
+    # print(data_path)
+    x = pickle.load(open(data_path, 'rb'))
+    return x
+    
+    
 def load_data() -> Optional[pd.DataFrame]:
     """
     TODO - Add scraper
@@ -10,8 +18,10 @@ def load_data() -> Optional[pd.DataFrame]:
     # temp
     
     file_path = Path(__file__).resolve()
-    data_path = file_path.parents[2].joinpath('data.csv')
-
+    # print("TU", file_path)
+    data_path = file_path.parents[3].joinpath('data','data.csv')
+    # print(data_path)
+    
     if not data_path.is_file():
         return None
 
